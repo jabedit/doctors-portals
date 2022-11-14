91a5190011e9fa40ca5a231631d42dba6f8d3322
jabedit <sylhet.bd321@gmail.com>
2022-11-14 19:50:28 +0600

    Available Apointment implement done !

--- src/Pages/Apointment/ApointmentBanner/ApointmentBanner.js
+++ src/Pages/Apointment/ApointmentBanner/ApointmentBanner.js
@@ -10,18 +10,18 @@ const ApointmentBanner = ({selectedDate, setSelectedDate}) => {
   return (
     <div className="hero min-h-screen ">
       <div className=" grid grid-cols-1 md:grid-cols-2  gap-10">
-        
-        <div className=" flex justify-center">
-          <DayPicker mode="single" selected={selectedDate} onSelect={setSelectedDate} />
-         
-        </div>
-        <div className="flex  flex-col-reverse">
+      <div className="flex  flex-col-reverse">
           <img
             src={chaire}
             className=" rounded-lg shadow-2xl  "
             alt=""
           />
         </div>
+        <div className=" flex justify-center  ">
+          <DayPicker mode="single" selected={selectedDate} onSelect={setSelectedDate} />
+         
+        </div>
+       
       </div>
     </div>
   );
--- src/Pages/Apointment/AvailableApointment/AvailableApointment.js
+++ src/Pages/Apointment/AvailableApointment/AvailableApointment.js
@@ -1,11 +1,24 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {format} from 'date-fns'
+import ApointmentOption from './ApointmentOption';
 const AvailableApointment = ({selectedDate}) => {
+    const [apointmentOptions, setApointmentOptions] = useState([])
+    useEffect(()=>{
+        fetch('apointmentOption.json')
+        .then(res => res.json())
+        .then(data => setApointmentOptions(data))
+    }, [])
     return (
-        <div className=' text-center text-secondary text-3xl font-bold'>
+        <section className='mb-10'>
+            <div className=' text-center text-secondary text-2xl font-bold mb-8'>
             <h2> Avalibale Apointment on {format(selectedDate, 'PP')}</h2>
         </div>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-5' >
+            {apointmentOptions.map(apointmentOption =>  <ApointmentOption apointmentOption={apointmentOption} />)}
+        </div>
+        </section>
+        
     );
 };
 
